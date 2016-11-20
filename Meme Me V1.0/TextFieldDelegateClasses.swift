9c30cb39c2a75b6b5b64708057fa3570a560b687
Michael Doroff <michaeldoroff@Michaels-MacBook-Air.local>
2016-11-20 11:15:36 -0600

    Add TextFields and Delegates

---
+++ Meme Me V1.0/TextFieldDelegateClasses.swift
@@ -0,0 +1,20 @@
+//
+//  TextFieldDelegateClasses.swift
+//  Meme Me V1.0
+//
+//  Created by Michael Doroff on 11/20/16.
+//  Copyright © 2016 Michael Doroff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeTextFieldDelegateClass: NSObject, UITextFieldDelegate {
+    
+    
+    
+    
+    
+    
+}
+
--- Meme Me V1.0/ViewController.swift
+++ Meme Me V1.0/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField1: UITextField!
+    
+    let MemeDelegate = MemeTextFieldDelegateClass()
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        textField1.delegate = MemeDelegate
+        textField2.delegate = MemeDelegate
     }
 
 
+    @IBAction func callPickAnImageFromCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    
+        
+    
+    
+    
+    }
+
+    @IBAction func callPickAnImageViewController(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    
+    
+    }
+
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            print(image)
+            imagePickerView.image = image
+            dismiss(animated: true, completion: nil)
+        }
+
+    }
 }
 
